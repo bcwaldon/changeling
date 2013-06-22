d868e33dc20010e423d930b50923b80d891400e3
Brian Waldon <bcwaldon@gmail.com>
2013-06-22 16:14:14 -0700

    Add config.py - use yaml config format

---
+++ changeling/config.py
@@ -0,0 +1,23 @@
+import copy
+
+import yaml
+
+
+DEFAULTS = {
+    's3.bucket': '3ac5d7b9-c711-4307-bc06-7668b3c26efd:changeling',
+}
+
+
+def _open_config_file(path):
+    return open(path, 'r')
+
+
+def _parse_config_file(fap):
+    return yaml.load(fap)
+
+
+def load(config_path):
+    fap = _open_config_file(config_path)
+    config = copy.deepcopy(DEFAULTS)
+    config.update(_parse_config_file(fap))
+    return config
