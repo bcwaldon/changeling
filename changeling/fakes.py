e5edf011afe3da842b05aaeaf54142dd6ca93c0f
Brian Waldon <bcwaldon@gmail.com>
2013-06-23 21:29:32 -0700

    Expand integration testing of changes

--- changeling/fakes.py
+++ changeling/fakes.py
@@ -1,8 +1,19 @@
 
+import changeling.exception
+
 
 class FakeStorage(object):
     def __init__(self, changes):
-        self.changes = changes
+        self.changes = [(c['id'], c.to_dict()) for c in changes]
 
     def list_changes(self):
-        return self.changes
+        return [c[1] for c in self.changes]
+
+    def get_change(self, change_id):
+        try:
+            return dict(self.changes)[change_id]
+        except KeyError:
+            raise changeling.exception.ChangeNotFound(change_id)
+
+    def get_change_history(self, change):
+        return []
--- test/integration/test_wsgi.py
+++ test/integration/test_wsgi.py
@@ -1,4 +1,6 @@
+import json
 import unittest
+import uuid
 
 import webob
 
@@ -6,17 +8,77 @@ import changeling.fakes
 import changeling.wsgi
 
 
-class TestWSGI(unittest.TestCase):
+
+class TestEmptyService(unittest.TestCase):
     def setUp(self):
         storage = changeling.fakes.FakeStorage([])
-        change_api_factory = changeling.api.change_api_factory
-        auth_api_factory = changeling.api.auth_api_factory
-
         self.app = changeling.wsgi.build_app(storage,
-                                             change_api_factory,
-                                             auth_api_factory)
+                                             changeling.api.change_api_factory,
+                                             changeling.api.auth_api_factory)
 
     def test_list_changes(self):
         req = webob.Request.blank('/changes')
         resp = req.get_response(self.app)
-        self.assertEqual(resp.status_code, 200)
+        self.assertEqual(200, resp.status_code)
+        self.assertEqual([], json.loads(resp.body))
+
+    def test_get_unknown_change(self):
+        req = webob.Request.blank('/changes/%s' % uuid.uuid4())
+        resp = req.get_response(self.app)
+        self.assertEqual(404, resp.status_code)
+
+    def test_patch_unknown_change(self):
+        req = webob.Request.blank('/changes/%s' % uuid.uuid4(),
+                                  method='PATCH', body="[]",
+                                  headers={'content-type': 'application/json'})
+        resp = req.get_response(self.app)
+        self.assertEqual(404, resp.status_code)
+
+    def test_delete_unknown_change(self):
+        req = webob.Request.blank('/changes/%s' % uuid.uuid4(),
+                                  method='DELETE')
+        resp = req.get_response(self.app)
+        self.assertEqual(404, resp.status_code)
+
+    def test_get_unknown_change_history(self):
+        req = webob.Request.blank('/changes/%s/history' % uuid.uuid4())
+        resp = req.get_response(self.app)
+        self.assertEqual(404, resp.status_code)
+
+
+class TestLoadedService(unittest.TestCase):
+    def setUp(self):
+        self.fixtures = [
+            changeling.models.Change(name='ping'),
+            changeling.models.Change(name='pong', tags=['foo', 'bar']),
+        ]
+        storage = changeling.fakes.FakeStorage(self.fixtures)
+        self.app = changeling.wsgi.build_app(storage,
+                                             changeling.api.change_api_factory,
+                                             changeling.api.auth_api_factory)
+        self.app.debug = True
+
+    def test_list_changes(self):
+        req = webob.Request.blank('/changes')
+        resp = req.get_response(self.app)
+
+        self.assertEqual(200, resp.status_code)
+
+        expected = [c['id'] for c in self.fixtures]
+        actual = [c['id'] for c in json.loads(resp.body)]
+        self.assertEqual(expected, actual)
+
+    def test_get_change(self):
+        change_id = self.fixtures[0]['id']
+        req = webob.Request.blank('/changes/%s' % change_id)
+        resp = req.get_response(self.app)
+
+        self.assertEqual(200, resp.status_code)
+        self.assertEqual(change_id, json.loads(resp.body)['id'])
+
+    def test_get_change_history(self):
+        url = '/changes/%s/history' % self.fixtures[0]['id']
+        req = webob.Request.blank(url)
+        resp = req.get_response(self.app)
+        self.assertEqual(200, resp.status_code)
+        self.assertEqual([], json.loads(resp.body))
